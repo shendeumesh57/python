82315554b02b752f39366324852b264aa74d9a27
shendeumesh <shendeumesh57@gmail.com>
2025-05-12 03:36:24 +0530

    098

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import re
 import urllib.parse
 import socket
 
-root_dir = sys.argv[1]
+root_dir = '/home/umesh/scripts/httpd'
 patterrn = re.compile(r"((http|https|ajp|ws|wss):\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,10}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*))")
 
 rx_blanks=re.compile(r"\W+") # to remove blanks and newlines
 
-for filename in glob.iglob(root_dir + '**/*',recursive=True):
+for filename in glob.iglob(root_dir + '**/**',recursive=True):
     #print("file: ",filename)
     if os.path.isfile(filename):
         # file exists
@@ -19,7 +19,7 @@ for filename in glob.iglob(root_dir + '**/*',recursive=True):
                 #print(line)
                 for url in re.findall(patterrn, line):
                     #print(url[0])
-                    url_parsed = urllib.parse.urlparse(url)
+                    url_parsed = urllib.parse.urlparse(url[0])
                     #scheme= 'scheme://netloc/path;parameters? query#fragment', allow_fragments=True
                     #print (url_parsed.port)
                     #print (url_parsed.hostname)
